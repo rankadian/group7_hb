73633addd702ec27441020a201d8206e46d2f656
unknown <dianthafafian@gmail.com>
2023-10-11 15:53:45 +0700

    first commit

---
+++ hbgroup7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/**
+ * hbgroup7
+ */
+public class hbgroup7 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean typeRoom;
+        boolean deluxe;
+        boolean executive;
+        int price;
+        double disc15=0.15;
+        double disc20=0.2;
+        double disc30=0.3;
+
+        System.out.println("Choose type of room : ");
+        typeRoom=input.nextBoolean();
+    }
+}
